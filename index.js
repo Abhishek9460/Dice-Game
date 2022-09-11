23d2f7a240d9afe258c346ff0e58a38844dde0f2
Abhishek9460 <98384405+Abhishek9460@users.noreply.github.com>
2022-09-11 12:56:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomImage1="dice"+randomNumber1+".png";
 document.querySelector(".img1").setAttribute("src",randomImage1);
 var randomNumber2=Math.random();
 randomNumber2=Math.floor(randomNumber2*6)+1;
-var randomImage2="images/dice"+randomNumber2+".png";
+var randomImage2="dice"+randomNumber2+".png";
 document.querySelector(".img2").setAttribute("src",randomImage2);
 if (randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="Player 1 Wins 🚩"
